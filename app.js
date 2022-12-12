bd2e5e506477a9f433d04096511e76af69c27768
abou <abou.mlanao@gmail.com>
2022-12-12 15:28:01 +0100

    add monogoose

--- app.js
+++ app.js
@@ -13,3 +13,21 @@ app.get('/api/:id', (req, res) => {
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`);
 });
+
+
+const mongoose = require('mongoose');
+
+const Vehicules = require('./vehicules');
+const Commandes = require('./commandes');
+
+// let port = 80;
+// app.listen(port, () => {            // ecoute du serveur sur le port 7878
+//     console.log('le serveur fonctionne sur le port 80')
+// })
+
+mongoose.connect(
+    process.env.DB_URL
+    , err => {
+        if (err) throw 'erreur est : ', err;
+        console.log('connected to MongoDB')
+    });
